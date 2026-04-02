acbcddf150f560545efc647b70a66d2223e1d40c
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2026-04-02 22:53:16 +0530

    Add Inventory model

---
+++ invenza-backend/src/main/java/com/invenza/dto/Inventory.java
@@ -0,0 +1,34 @@
+package com.invenza.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "inventory")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inventoryId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
