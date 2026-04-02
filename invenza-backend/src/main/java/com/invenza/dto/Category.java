448c6b278fd815f0810f341cf26e48cf397efd79
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2026-04-02 22:54:35 +0530

    Add Category model

---
+++ invenza-backend/src/main/java/com/invenza/dto/Category.java
@@ -0,0 +1,25 @@
+package com.invenza.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Integer defaultShelfLifeDays;
+}
