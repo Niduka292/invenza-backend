9840ef06ffadabd7b89e00c01e378c38631a948c
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2026-04-02 22:48:47 +0530

    Complete project structure

---
+++ invenza-backend/src/main/java/com/invenza/InvenzaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.invenza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenzaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvenzaBackendApplication.class, args);
+        System.out.println("Application successfully started");
+    }
+
+}
