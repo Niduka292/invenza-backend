f15e51d0f3ab7197916bff3210bb430a6eec1438
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2026-04-07 23:19:56 +0530

    Create User request and response DTOs

---
+++ invenza-backend/src/main/java/com/invenza/dto/UserResponseDTO.java
@@ -0,0 +1,20 @@
+package com.invenza.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDTO {
+
+    private Long userId;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+}
